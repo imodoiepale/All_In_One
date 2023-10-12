b0e0c0743173b26926ee4563100a94e9f00cc89c
ijepale@gmail.com <ijepale@gmail.com>
2023-10-12 11:29:47 +0300

    popup v1

---
+++ CHROME EXTENSIONS/James/popup.js
@@ -0,0 +1,15 @@
+// Add your logic for website and client selection here
+document.getElementById('clientSearch').addEventListener('input', function (event) {
+    // Handle client search input
+    const searchQuery = event.target.value;
+    // Perform search and update UI
+  });
+  
+  // Add logic for website selection
+  
+  // Fetch and display client data based on the selected client
+  function fetchAndDisplayClientData(clientId) {
+    // Use fetch or other methods to get client data from your server
+    // Update dataPreview table with the retrieved data
+  }
+  
